2c066e670e28acf8666fc238e140ec9c19b5eb2b
Angelo Sanchez <jsanchezs17@upao.edu.pe>
2025-04-19 21:39:45 -0500

    Adicionales de Especialista: Dias de fiebre

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -12,12 +12,17 @@ import {Router} from '@angular/router';
 export class FormComponent implements OnInit {
   form!: FormGroup;
   currentPage = 1;
-  totalPages = 3;  // Tres páginas para dividir las preguntas
+  totalPages = 2;  // Tres páginas para dividir las preguntas
+  edades: number[] = [];
+  diasDeFiebre: number[] = [];  // Lista para los días de fiebre
 
   constructor(private fb: FormBuilder, private router: Router, private http: HttpClient) {}
 
   ngOnInit() {
     this.form = this.fb.group({
+      edad: [null, Validators.required],
+      genero: ['', Validators.required],
+      diasDeFiebre: [null, Validators.required],
       dolorCabezaSevero: ['', Validators.required],
       dolorDetrasOjos: ['', Validators.required],
       dolorArticularMuscular: ['', Validators.required],
@@ -27,6 +32,9 @@ export class FormComponent implements OnInit {
       nauseasVomitos: ['', Validators.required],
       diarrea: ['', Validators.required]
     });
+
+    this.edades = Array.from({ length: 99 }, (_, i) => i + 1);
+    this.diasDeFiebre = Array.from({ length: 15 }, (_, i) => i);
   }
 
   siguiente() {
@@ -45,6 +53,9 @@ export class FormComponent implements OnInit {
     if (this.form.valid) {
       const formData = this.form.value;
       const formDataWithBooleans = {
+        edad: formData.edad,
+        genero: formData.genero,
+        dias_de_fiebre: formData.diasDeFiebre,
         dolor_cabeza_severo: formData.dolorCabezaSevero === 'Si',
         dolor_detras_ojos: formData.dolorDetrasOjos === 'Si',
         dolor_articular_muscular: formData.dolorArticularMuscular === 'Si',
@@ -55,11 +66,13 @@ export class FormComponent implements OnInit {
         diarrea: formData.diarrea === 'Si'
       };
 
+      console.log('Datos del formulario:', formDataWithBooleans);
+
       // Enviar los datos
       this.http.post('http://localhost:8000/evaluar_riesgo', formDataWithBooleans)
         .subscribe((response: any) => {
           // Recibimos la respuesta y redirigimos al componente de resultados con el riesgo
-          this.router.navigate(['/result'], { state: { resultado: response.riesgo } });
+          this.router.navigate(['/result'], { state: { resultado: response.riesgo, probabilidad: response.probabilidad } });
         }, (error) => {
           console.error('Error en la solicitud:', error);
         });
