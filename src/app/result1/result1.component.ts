f944b04661622411b51286b0f2977044fab66763
Angelo Sanchez <jsanchezs17@upao.edu.pe>
2025-06-02 23:18:57 -0500

    Actualizacion de vista de resultado + descarga pdf

--- src/app/result1/result1.component.ts
+++ src/app/result1/result1.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-result1',
@@ -22,6 +24,65 @@ export class Result1Component implements OnInit {
       console.log('Datos recibidos en ResultComponent:', state);  // Verifica los datos recibidos
       this.riesgo_random_forest = state.riesgo_random_forest;
 
+    }
+  }
+
+  descargarPDF() {
+    const data = document.getElementById('pdfContent');
+
+    if (data) {
+      html2canvas(data).then(canvas => {
+        const contentImg = canvas.toDataURL('image/png');
+        const pdf = new jsPDF('p', 'mm', 'a4');
+
+        const pageWidth = pdf.internal.pageSize.getWidth();
+        const headerImg = 'header.png'; // o base64
+        const headerImage = new Image();
+        headerImage.src = headerImg;
+
+        headerImage.onload = () => {
+          const headerMaxWidth = pageWidth * 0.9;
+          const ratio = headerImage.height / headerImage.width;
+          const headerHeight = headerMaxWidth * ratio;
+          const headerX = (pageWidth - headerMaxWidth) / 2;
+          const headerY = 10;
+
+          //  Header
+          pdf.addImage(headerImage, 'PNG', headerX, headerY, headerMaxWidth, headerHeight);
+
+          // Contenido HTML
+          const contentY = headerY + headerHeight + 10;
+          const imgProps = pdf.getImageProperties(contentImg);
+          const contentWidth = pageWidth * 0.8;
+          const contentHeight = (imgProps.height * contentWidth) / imgProps.width;
+          const contentX = (pageWidth - contentWidth) / 2;
+
+          pdf.addImage(contentImg, 'PNG', contentX, contentY, contentWidth, contentHeight);
+
+          // Fecha y hora
+          const now = new Date();
+          const fechaHora = now.toLocaleString('es-PE', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false
+          });
+
+          // Calcular posición Y después del contenido
+          const fechaY = contentY + contentHeight + 20;
+
+          // Fecha y hora debajo del contenido
+          pdf.setFontSize(11);
+          pdf.setTextColor(80);
+          pdf.text(`Fecha y hora: ${fechaHora}`, 15, fechaY);
+
+          // Descargar
+          pdf.save('resultado.pdf');
+        };
+      });
     }
   }
 
