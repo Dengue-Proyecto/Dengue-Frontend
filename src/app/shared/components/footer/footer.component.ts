12faa635ecfdb4eab8fdf72243d34fa7bf8baf9d
Angelo Sanchez <jsanchezs17@upao.edu.pe>
2025-10-24 16:08:28 -0500

    agrega footer con politicas y eliminar cuenta

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../core/auth/auth.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  mostrarModalPolitica: boolean = false;
+  mostrarModalFlujo: boolean = false;
+  mostrarModalEliminarCuenta: boolean = false;
 
+  constructor(public authService: AuthService) {}
+
+  abrirModalPolitica() {
+    this.mostrarModalPolitica = true;
+    document.body.style.overflow = 'hidden';
+  }
+
+  cerrarModalPolitica() {
+    this.mostrarModalPolitica = false;
+    document.body.style.overflow = 'auto';
+  }
+
+  abrirModalFlujo() {
+    this.mostrarModalFlujo = true;
+    document.body.style.overflow = 'hidden';
+  }
+
+  cerrarModalFlujo() {
+    this.mostrarModalFlujo = false;
+    document.body.style.overflow = 'auto';
+  }
+
+  abrirModalEliminarCuenta() {
+    this.mostrarModalEliminarCuenta = true;
+    document.body.style.overflow = 'hidden';
+  }
+
+  cerrarModalEliminarCuenta() {
+    this.mostrarModalEliminarCuenta = false;
+    document.body.style.overflow = 'auto';
+  }
+
+  confirmarEliminarCuenta() {
+    console.log('Eliminar cuenta confirmado');
+    this.cerrarModalEliminarCuenta();
+    // TODO: Implementar la eliminación de cuenta
+  }
 }
